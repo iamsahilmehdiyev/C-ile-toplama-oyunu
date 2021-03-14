b0d9d460a69113e3e339b3b224db8d1649b8991d
Mehdiyev111 <sahil.mehdiyev2000@gmailc.com>
2021-03-14 22:06:38 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Random rnd = new Random();
+            int ilkSayi = 0, ikinciSayi = 0, toplamPuan = 0, sayilarinToplami = 0, girilenCevap = 0, dogruCevapSayisi = 0, yanlisCevapSayisi = 0;
+            char devam;
+            do
+            {
+
+                ilkSayi = rnd.Next(1, 100);
+                ikinciSayi = rnd.Next(1, 100);
+                sayilarinToplami = ilkSayi + ikinciSayi;
+                Console.WriteLine("{0} + {1} = ?", ilkSayi, ikinciSayi);
+                Console.Write("Cevabınız : ");
+                girilenCevap = Convert.ToInt32(Console.ReadLine());
+                if (girilenCevap == sayilarinToplami)
+                {
+                    Console.WriteLine("Tebrikler Bildiniz");
+                    toplamPuan += 5;
+                    dogruCevapSayisi += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Üzgünüm Bilemediniz");
+                    toplamPuan -= 2;
+                    yanlisCevapSayisi += 1; 
+                }
+                Console.Write("Tekrar Oynamak istiyormusunuz(evet veya hayir)?");
+                devam = Convert.ToChar(Console.ReadLine());
+
+            } while (devam == 'E' || devam == 'e');
+
+            Console.WriteLine("Toplam Doğru Cevap Sayınız {0}, Yanlış Cevap Sayınız {1}, Toplamda Almış Olduğunuz Puan {2}", dogruCevapSayisi, yanlisCevapSayisi, toplamPuan);
+
+        }
+    }
+}
